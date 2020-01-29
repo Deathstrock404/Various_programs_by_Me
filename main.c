7aab10d81366cb58b5ce5c84285d3961bde6346c
Kamaljot Singh <48052931+Deathstrock404@users.noreply.github.com>
2020-01-29 21:40:39 +0530

    bubble sort algorithm
    in this program we use c language to perform bubble sort on an array

--- main.c
+++ main.c
@@ -1,117 +1,37 @@
-/*
-*	Banker's Algorithm
-*
-*       Author 	 : Kamaljot Singh
-*
-*	Language : C
-*
-*	compiler : gcc
-*
-*/
-
-/*<--------------PREPROCESSING STATEMENTS--------------->*/
-#include <stdio.h>
-#include <conio.h>
-
-/*<--------------MAIN FUNCTION--------------->*/
+//program to perform bubble sort
+#include<stdio.h>
 
 int main()
 {
-int Max[10][10], need[10][10], alloc[10][10], avail[10], completed[10], safeSequence[10];
-int p, r, i, j, process, count;
-count = 0;
 
-printf("Enter the no of processes : ");
-scanf("%d", &p);
+   int count, temp, i, j, number[30];
 
-for(i = 0; i< p; i++)
-	completed[i] = 0;
+   printf("How many numbers are u going to enter?: ");
+   scanf("%d",&count);
 
-printf("\n\nEnter the no of resources : ");
-scanf("%d", &r);
+   printf("Enter %d numbers: ",count);
+//entering the elements in the array
+   for(i=0;i<count;i++)
+   scanf("%d",&number[i]);
 
-printf("\n\nEnter the Max Matrix for each process : ");
-for(i = 0; i < p; i++)
-{
-	printf("\nFor process %d : ", i + 1);
-	for(j = 0; j < r; j++)
-		scanf("%d", &Max[i][j]);
-}
+   // This is the main logic of bubble sort algorithm
 
-printf("\n\nEnter the allocation for each process : ");
-for(i = 0; i < p; i++)
-{
-	printf("\nFor process %d : ",i + 1);
-	for(j = 0; j < r; j++)
-		scanf("%d", &alloc[i][j]);
-}
+   for(i=count-2;i>=0;i--)
+    {
+      for(j=0;j<=i;j++)
+      {
+        if(number[j]>number[j+1])
+        {
+           temp=number[j];
+           number[j]=number[j+1];
+           number[j+1]=temp;
+        }
+      }
+   }
 
-printf("\n\nEnter the Available Resources : ");
-for(i = 0; i < r; i++)
-		scanf("%d", &avail[i]);
+   printf("Sorted elements: ");
+   for(i=0;i<count;i++)
+      printf(" %d",number[i]);
 
-
-	for(i = 0; i < p; i++)
-		for(j = 0; j < r; j++)
-			need[i][j] = Max[i][j] - alloc[i][j];
-
-do
-{
-	printf("\n Max matrix:\tAllocation matrix:\n");
-	for(i = 0; i < p; i++)
-	{
-		for( j = 0; j < r; j++)
-			printf("%d  ", Max[i][j]);
-		printf("\t\t");
-		for( j = 0; j < r; j++)
-			printf("%d  ", alloc[i][j]);
-		printf("\n");
-	}
-
-	process = -1;
-
-	for(i = 0; i < p; i++)
-	{
-		if(completed[i] == 0)//if not completed
-		{
-			process = i ;
-			for(j = 0; j < r; j++)
-			{
-				if(avail[j] < need[i][j])
-				{
-					process = -1;
-					break;
-				}
-			}
-		}
-		if(process != -1)
-			break;
-	}
-
-	if(process != -1)
-	{
-		printf("\nProcess %d runs to completion!", process + 1);
-		safeSequence[count] = process + 1;
-		count++;
-		for(j = 0; j < r; j++)
-		{
-			avail[j] += alloc[process][j];
-			alloc[process][j] = 0;
-			Max[process][j] = 0;
-			completed[process] = 1;
-		}
-	}
-}while(count != p && process != -1);
-
-if(count == p)
-{
-	printf("\nThe system is in a safe state!!\n");
-	printf("Safe Sequence : < ");
-	for( i = 0; i < p; i++)
-			printf("%d  ", safeSequence[i]);
-	printf(">\n");
-}
-else
-	printf("\nThe system is in an unsafe state!!");
-getch();
+   return 0;
 }
