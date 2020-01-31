9f1d3875625a1eec2c79ec8f1ddd3f8d49de7164
Kamaljot Singh <48052931+Deathstrock404@users.noreply.github.com>
2020-01-31 22:19:09 +0530

    the use of char and Boolean
    in this program we show how to use char and Boolean in a java program

--- Main.java
+++ Main.java
@@ -1,18 +1,10 @@
-package com.kamaljot;
+package com.company;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        int Quantity_of_the_eggs = 1342;
-        System.out.println("the total no of the eggs present with the user is " + Quantity_of_the_eggs);
-        int gross = Quantity_of_the_eggs/144;
-        System.out.println("Gross is equal to " + gross);
-        int temp = Quantity_of_the_eggs % 144;
-        // this is used to store value of the reminder but we don't want to display the value of the remainder
-        int Dozen = temp/12;
-        System.out.println("the no of dozen " + Dozen);
-        int Remaining = temp % 12;
-        System.out.println("the no of remaining eggs is " + Remaining);
+        //width is = 16
+        char a = '\u00AE';
+        System.out.println(a);
     }
-}
+    }
