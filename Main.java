769e29f9fca5fbf3b60365fbb5969ab174f8b463
Kamaljot Singh <48052931+Deathstrock404@users.noreply.github.com>
2020-02-01 21:39:46 +0530

    use of  float and double

--- Main.java
+++ Main.java
@@ -3,8 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        //width is = 16
-        char a = '\u00AE';
-        System.out.println(a);
+        //width of int = 34(4 byte)
+        int a = 5/2;
+        // width of float = 32(4 byte)
+        float b =5f/3f;
+        //width of double = 64(8 byte)
+        double c = 5d / 3d;
+        System.out.println("my a ="+a);
+        System.out.println("my b ="+b);
+        System.out.println("my c ="+c);
     }
-    }
+}
