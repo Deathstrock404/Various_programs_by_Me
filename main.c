6ec824ea84ae820125ff0c2773f8136b450f27d6
Kamaljot Singh <48052931+Deathstrock404@users.noreply.github.com>
2020-02-04 12:16:33 +0530

    the fcfs algorithm

--- main.c
+++ main.c
@@ -1,37 +1,43 @@
-//program to perform bubble sort
 #include<stdio.h>
 
 int main()
 {
+    int n,bt[20],wt[20],tat[20],avwt=0,avtat=0,i,j;
+    printf("Enter total number of processes(maximum 20):");
+    scanf("%d",&n);
 
-   int count, temp, i, j, number[30];
-
-   printf("How many numbers are u going to enter?: ");
-   scanf("%d",&count);
-
-   printf("Enter %d numbers: ",count);
-//entering the elements in the array
-   for(i=0;i<count;i++)
-   scanf("%d",&number[i]);
-
-   // This is the main logic of bubble sort algorithm
-
-   for(i=count-2;i>=0;i--)
+    printf("\nEnter Process Burst Time\n");
+    for(i=0;i<n;i++)
     {
-      for(j=0;j<=i;j++)
-      {
-        if(number[j]>number[j+1])
-        {
-           temp=number[j];
-           number[j]=number[j+1];
-           number[j+1]=temp;
-        }
-      }
-   }
+        printf("P[%d]:",i+1);
+        scanf("%d",&bt[i]);
+    }
 
-   printf("Sorted elements: ");
-   for(i=0;i<count;i++)
-      printf(" %d",number[i]);
+    wt[0]=0;    //waiting time for first process is 0
 
-   return 0;
+    //calculating waiting time
+    for(i=1;i<n;i++)
+    {
+        wt[i]=0;
+        for(j=0;j<i;j++)
+            wt[i]+=bt[j];
+    }
+
+    printf("\nProcess\t\tBurst Time\tWaiting Time\tTurnaround Time");
+
+    //calculating turnaround time
+    for(i=0;i<n;i++)
+    {
+        tat[i]=bt[i]+wt[i];
+        avwt+=wt[i];
+        avtat+=tat[i];
+        printf("\nP[%d]\t\t%d\t\t%d\t\t%d",i+1,bt[i],wt[i],tat[i]);
+    }
+
+    avwt/=i;
+    avtat/=i;
+    printf("\n\nAverage Waiting Time:%d",avwt);
+    printf("\nAverage Turnaround Time:%d",avtat);
+
+    return 0;
 }
