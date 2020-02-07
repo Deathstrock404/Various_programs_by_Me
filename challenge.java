fb4371510bb2b084c6ae97e99e865d0f440810ee
Kamaljot Singh <48052931+Deathstrock404@users.noreply.github.com>
2020-02-07 22:33:02 +0530

    use of float and double in java
    in this program we show the use of float and double data type in java

---
+++ challenge.java
@@ -0,0 +1,7 @@
+public class challenge {
+    public static void main(String[] args){
+        double pound = 200;
+        double kilo=pound*0.45_359_237d;
+        System.out.println("the convertion of pound to kilo is"+kilo);
+    }
+}
