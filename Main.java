648a929250301a52bc6da32b2465eb3c3af114bb
Kamaljot Singh <48052931+Deathstrock404@users.noreply.github.com>
2020-02-10 21:50:19 +0530

    use of modules in java
    in this program i have learned how to use modules in a java program

--- Main.java
+++ Main.java
@@ -3,20 +3,32 @@ package com.kamaljot;
 public class Main {
 
     public static void main(String[] args) {
-        //a statement can be a line of code, variable declaration, mostly ";" is required to complete a statement
-        int myVariable = 50;
-        myVariable++;
-        myVariable--;
-        System.out.println("this is a test");
+        int highScore = calculateScore(true, 800, 5, 100);
+        System.out.println("your final score was " + highScore);
 
-        System.out.println("this is " +
-                "another " +
-                "still more");
-        //whitespace is the space required between operators or any other data
-        // so its more easier to read
 
-        //indentation
-        //its required to differentiate various code blocks
+        highScore = calculateScore(true, 10000, 8, 200);
+        System.out.println(" your score was " + highScore);
 
+
+        int totalScore = latest(true, 2500, 5460, 6969, 5847);
+        System.out.println("the total of all the above elements is " + totalScore);
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int levelComp, int bonus) {
+        if (gameOver == true) {
+            int finalScore = score + (levelComp * bonus);
+            finalScore += 2000;
+            return finalScore;
+        }
+        return (-1);
+    }
+
+    public static int latest(boolean total, int kamaljot, int vaibhav, int anshul, int ankush) {
+        if (total == true) {
+            int totalScore = ((kamaljot) + (vaibhav) + (anshul) + (ankush));
+            return (totalScore);
+        }
+        return -1;
     }
 }
