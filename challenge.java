2f3e94ec14f369abd3f38ab2360d96a536771eb2
Kamaljot Singh <48052931+Deathstrock404@users.noreply.github.com>
2020-02-13 22:33:00 +0530

    this was a challenge on modules in java
    this program was given to me as a challenge so as i could understand the concept of modules in java language

--- challenge.java
+++ challenge.java
@@ -1,7 +1,64 @@
 public class challenge {
-    public static void main(String[] args){
-        double pound = 200;
-        double kilo=pound*0.45_359_237d;
-        System.out.println("the convertion of pound to kilo is"+kilo);
+
+    public static void main(String[] args) {
+        int highScore = calculateScore(true, 800, 5, 100);
+        System.out.println("your final score was " + highScore);
+
+
+
+        highScore = calculateScore(true, 10000, 8, 200);
+        System.out.println(" your score was " + highScore);
+
+
+        int highScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("kamal",highScorePosition);
+
+
+
+
+
+
+        highScorePosition = calculateHighScorePosition(900);
+        displayHighScorePosition("anshul",highScorePosition);
+
+
+
+        highScorePosition = calculateHighScorePosition(400);
+        displayHighScorePosition("ankush",highScorePosition);
+
+
+
+
+        highScorePosition = calculateHighScorePosition(50);
+        displayHighScorePosition("jatin",highScorePosition);
+
+    }
+
+    public static void displayHighScorePosition(String playerName, int highScorePosition){
+        System.out.println(playerName + " managed to get into position " + highScorePosition + "on the high score table");
+    }
+
+    public static int calculateHighScorePosition(int playerScore){
+        if(playerScore >= 1000){
+            return 1;
+        }
+        else if (playerScore >= 500){
+            return 2;
+        }
+        else if(playerScore >= 100 ){
+            return 3;
+        }
+            return 4;
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int levelComp, int bonus) {
+        if (gameOver == true) {
+            int finalScore = score + (levelComp * bonus);
+            finalScore += 2000;
+            return finalScore;
+        }
+        return (-1);
     }
 }
+
+
