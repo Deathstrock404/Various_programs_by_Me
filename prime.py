ec63e6fd7dfecef5c8b626df3f7ac82cef7725d3
Kamaljot Singh <48052931+Deathstrock404@users.noreply.github.com>
2020-02-19 21:51:45 +0530

    how to check if a no is prime or not
    This program was written in the python language this program is to check whether a no is prime or not

---
+++ prime.py
@@ -0,0 +1,9 @@
+'''program to find if a no is prime or not'''
+x=int(input())
+
+for n in range(2,x-1):
+    if x % = 0:
+        print(x is not a prime no)
+        break
+    else:
+        print(n is a prime no)
