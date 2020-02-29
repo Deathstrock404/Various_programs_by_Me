012512f0cecc58a27120b2f604358431445f2a30
Kamaljot Singh <48052931+Deathstrock404@users.noreply.github.com>
2020-02-29 12:17:05 +0530

    program made in java lab
    this is a program made in the java lab during the java lecture

--- Main.java
+++ Main.java
@@ -1,37 +1,11 @@
-package com.kamaljot;
+public class Main
+{
 
-public class Main {
-
-    public static void main(String[] args) {
-        //list of various primitive data types
-        //int
-        //short
-        //byte
-        //long
-        //float
-        //char
-        //boolean
-        //double
-
-        String a="This is a string";
-        System.out.println(a);
-
-
-        a=a+", \u00A9 2015 and this is more";
-        System.out.println(a);
-
-
-        String c="550.55";
-        c=c+"49.55";
-        System.out.println(c);
-
-
-        String last = "10";
-        int mal=50;
-        last = last + mal;
-        System.out.println(last);
-        double dou = 525253.2555d;
-        last = last + dou;
-        System.out.println(last);
+    public static void main(String[] args)
+    //string is a class, args means arguments and command lines
+    //static means these are internally made function
+    //public means the range of access is that anyone can use the program
+    {
+        System.out.println("My First Project In Java In The Lab");
     }
 }
