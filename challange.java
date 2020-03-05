aa6afda9b547a1bde51f41176a0b257be44b401a
Kamaljot Singh <48052931+Deathstrock404@users.noreply.github.com>
2020-03-05 20:11:00 +0530

    this file contain java program

---
+++ challange.java
@@ -0,0 +1,16 @@
+public class challange {
+    public static void main(String[] args) {
+        byte a=10;
+        System.out.println(a);
+        short b=20;
+        System.out.println(b);
+        int c=50;
+        System.out.println(c);
+        long d=(long)(50000)+(10*(a+b+c));
+        short e=(short)((1000)+(10*(a+b+c)));
+        System.out.println(d);
+        System.out.println("the total of e ia:"+e);
+
+
+    }
+}
